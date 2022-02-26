c649491c2c757b3b1f75db0e683286d15b8ec6a4
machaoxue <machaoxue@yeah.net>
2022-02-26 15:53:38 +0800

    feat(js-code): 手写 Promise

---
+++ src/javascript/promise.js
@@ -0,0 +1,53 @@
+// 手写 promise 简化版
+
+class MyPromise {
+  #status = 'pending'
+  constructor(fn) {
+    this.list = []
+    const resolve = (data) => {
+      this.#status = 'resolved'
+      const p = this.list.shift()
+      if (p && p[0]) {
+        const res = p[0].call(undefined, data)
+        if (res instanceof MyPromise) {
+          res.then((data) => {
+            resolve(data)
+          }, () => {
+            reject(data)
+          })
+        }
+      }
+    }
+    const reject = (data) => {
+      this.#status = 'rejected'
+      const p = this.list.shift()
+      if (p && p[1]) {
+        const res = p[1].call(undefined, data)
+        if (res instanceof MyPromise) {
+          res.then((data) => {
+            resolve(data)
+          }, (data) => {
+            reject(data)
+          })
+        }
+      }
+    }
+    fn.call(undefined, resolve, reject)
+  }
+
+  then(fn1, fn2) {
+    this.list.push([fn1, fn2])
+  }
+}
+
+const promise = new MyPromise((resolve, reject) => {
+  setTimeout(function () {
+    reject('出错')
+  }, 3000)
+})
+
+promise.then(data => {
+  console.log(data)
+}, (data) => {
+  console.log(data)
+})
